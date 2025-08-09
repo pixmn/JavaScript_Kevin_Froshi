0f1ea3b7102552509004f612f02f8ab6cb04071c
Froshi <kg5892868@gmail.com>
2025-08-09 11:49:45 -0500

    ...

--- prompt.js
+++ prompt.js
@@ -1,5 +1,9 @@
-let palabra1 = prompt("Escribe una palabra a concatenar:");
-let palabra2 = prompt("Escribe otra palabra a concatenar:");
-let concatenar = palabra1 + palabra2;
-console.log(concatenar);
-alert("La palabra concatenada es: " + concatenar);
+let palabra1 = prompt("Escribe una palabra a sumar:");
+palabra1 = parseInt(palabra1);
+
+let palabra2 = prompt("Escribe otra palabra a sumar:");
+palabra2 = parseInt(palabra2);
+
+let suma = palabra1 + palabra2;
+console.log(suma);
+alert("La palabra sumada es: " + suma);
