e79ab8c1ed2ccaadd942cb2da5571c6b837d4312
Froshi <kg5892868@gmail.com>
2025-08-09 11:34:10 -0500

    cuarto

---
+++ prompt.js
@@ -0,0 +1,5 @@
+let palabra1 = prompt("Escribe una palabra a concatenar:");
+let palabra2 = prompt("Escribe otra palabra a concatenar:");
+let concatenar = palabra1 + palabra2;
+console.log(concatenar);
+alert("La palabra concatenada es: " + concatenar);
