036c2360969464141ee477872f9692de30cb6a76
Froshi <kg5892868@gmail.com>
2025-08-15 20:39:38 -0500

    15/08/2025

---
+++ opcomparacion.js
@@ -0,0 +1,14 @@
+let num1 = (8>2); //true
+console.log("8>2 = " + num1);
+
+let num2 = (2<1); // false
+console.log("2<1 = " + num2);
+
+let num3 = (2==1); //false 
+console.log("2==1 = " + num3);
+
+let num4 = (2=='2'); //true
+console.log("2=='2' = " + num4);
+
+let num5 = (2==='2'); //false
+console.log("2==='2' = " + num3);
---
+++ opternario.js
@@ -0,0 +1,13 @@
+console.log("7%2 usandose if y else");
+if (7 % 2 === 0) {
+  console.log("7%2 es impar");
+} else {
+  console.log("7%2 es par");
+}
+
+console.log("7%2 usandose comparador ternario");
+8 > 2 ? console.log("8>2 = si") : console.log("8>2 = no");
+2 < 1 ? console.log("2<1 = si") : console.log("2<1 = no");
+2 == 1 ? console.log("2==1 = si") : console.log("2==1 = no");
+2 == "2" ? console.log("2=='2' = si") : console.log("2=='2' = no");
+2 === "2" ? console.log("2==='2' = si") : console.log("2==='2' = no");
