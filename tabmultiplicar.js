5d70128d4d06acd55cb752863d15d0b9e41cab7c
Froshi <kg5892868@gmail.com>
2025-08-23 12:01:53 -0500

    23/08

--- tabmultiplicar.js
+++ tabmultiplicar.js
@@ -1,4 +1,6 @@
-let mult = prompt("Ingrese un numero, te mostraré la tabla de multiplicar de ese numero"); 
-    for (let i = 1; i < 10; i++){
-    alert(mult + " * " + i + " = " + (mult * i));}
-    
+let mult = prompt(
+  "Ingrese un numero, te mostraré la tabla de multiplicar de ese numero"
+);
+for (let i = 1; i < 10; i++) {
+  alert(mult + " * " + i + " = " + mult * i);
+}
